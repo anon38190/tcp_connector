3b9b7a6ff45ca5b0f071f02a47156f324358fb38
Dmitry Kovanikov <kovanikov@gmail.com>
2017-02-20 22:27:42 +0300

    [CSLD-15] Add basic transaction constructs

--- CSL_Structures.py
+++ CSL_Structures.py
@@ -1,9 +1,14 @@
 from construct import *
 
 # Constants
-DEFAULT_HASH_SIZE = 32
-PUB_KEY_SIZE = 32
+
+## Sizes
+DEFAULT_HASH_SIZE = 32  # hash size for Blake2s_256 hashing
+DEFAULT_PUBLIC_KEY_SIZE = 32
 DEFAULT_SIGNATURE_SIZE = 64
+DEFAULT_ADDRESS_HASH_SIZE = 28  # hash size for Blake2s_224 hashing
+
+## Protocol constants
 VERSION_MAGIC = 16842752
 PROTOCOL_MAGIC = 0
 MAJ_VER = 0
@@ -27,19 +32,118 @@ MSG_NAME_MAP = {"SysStartRequest":1001,
                 
 ENC_DATA = Struct("enc_data" / VarInt)
 
-# Structures
+#######################
+# COMMON HASKELL
+#######################
+
+def Pair(a, b):
+    return a >> b  # or better Struct("fst"/a, "snd"/b)?
+
+def Maybe(a):
+    """
+    >>> Maybe(VarInt).build(dict(tag=0, from_just=None))
+    b'\x00'
+    >>> Maybe(VarInt).build(dict(tag=1, from_just=3))
+    b'\x01\x03'
+
+    :param a: type of optional value
+    :return: structure that encodes `Maybe a`
+    """
+    return Struct("tag" / Int8ub,
+                  "from_just" / Switch(this.tag,
+                  {
+                    0: Terminated,  # TODO: avoid specify `fromJust=None` if tag=0
+                    1: a
+                  }))
+
+def List(a):
+    return PrefixedArray(VarInt, a)
+
+#######################
+# BASIC CARDANO
+#######################
+
+# TODO: some hardcore here...
+COIN = Struct()
+
+HASH = Array(DEFAULT_HASH_SIZE, Byte)
+PUBLIC_KEY = Array(DEFAULT_PUBLIC_KEY_SIZE, Byte)
+SIGNATURE = Array(DEFAULT_SIGNATURE_SIZE, Byte)
+
+SLOT_ID = "epoch_index" / VarInt + \
+          "slot_index" / VarInt
+
+EMPTY_ATTRIBUTES = List(Byte)
+
+# TODO: describe SCRIPT
+SCRIPT = Struct()
+
+# TODO: describe AddressAttributes
+ADDRESS_ATTRIBUTES = Struct()
+
+# TODO: describe Address
+ADDRESS = Struct()
+
+MERKLE_ROOT = HASH
+
+#######################
+# TRANSACTIONS MESSAGES
+#######################
+
+TX_IN = "hash" / HASH + \
+        "index" / VarInt
+
+TX_OUT = "address" / ADDRESS + \
+         "out_value" / COIN
+
+TX_OUT_AUX = Struct("output" / TX_OUT,
+                    "distr" / List(Pair(HASH, COIN)))
+
+TX_SIG_DATA = Struct("id" / HASH,
+                     "index" / Int32ub,
+                     "hash_out" / HASH,
+                     "hash_distr" / HASH)
+
+# >>> TX_IN_WITNESS.build(dict(tag=0, key=range(32), sig=range(64)))
+# b'\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12
+TX_IN_WITNESS = Struct("tag" / Int8ub,
+                       Embedded(Switch(this.tag,
+                       {
+                         0: Struct("key" / PUBLIC_KEY,
+                                   "sig" / SIGNATURE),
+                         1: Struct("validator" / SCRIPT,
+                                   "redeemer" / SCRIPT)
+                       })))
+
+TX_WITNESS = List(TX_IN_WITNESS)
+
+TRANSACTION = Struct("inputs" / List(TX_IN),
+                     "outputs" / List(TX_OUT),
+                     "attributes" / EMPTY_ATTRIBUTES)
+
+# TODO: describe distribution serialization logic
+TX_DISTRIBUTION = Struct()
+
+TX_AUX = Struct("tx" / TRANSACTION,
+                "witness" / TX_WITNESS,
+                "distribution" / TX_DISTRIBUTION)
+
+##################
+# Other Structures
+##################
+
 DATA_MSG = Struct("lwcid" / Int32ub,
                   "size" / Int32ub,
                   "msg" / Array(this.size, Byte))
-                  
+
 BID_CONTROL_HEADER = Struct("cc" / Byte,
                             "nonce" / Int64ub)
-                            
+
 UND_CONTROL_HEADER = Struct("cc" / Int8ub)
 
 GETBLOCKS = Struct("old_hash" / Array(DEFAULT_HASH_SIZE, Byte),
                    "new_hash" / Array(DEFAULT_HASH_SIZE, Byte))
-                   
+
 SYSSTARTREQUEST = Struct("null" / Byte)
 
 VERSIONREQ = Struct()
@@ -47,7 +151,7 @@ VERSIONREQ = Struct()
 GETHEADERS_RANGE = Struct("count" / Int8ub,
                           "old_hashes" / Array(this.count * DEFAULT_HASH_SIZE, Byte),
                           "new_hash" / Array(DEFAULT_HASH_SIZE, Byte))
-                          
+
 GETHEADERS_SINGLE = Struct("count" / Int8ub,
                            "old_hashes" / Array(this.count * DEFAULT_HASH_SIZE, Byte),
                            "new_hash" / Default(Byte, 0x00))
@@ -66,35 +170,27 @@ TIMESTAMP = Struct("size" / Int8ub,
 SYSSTARTRESPONSE = Struct("vers_magic" / Int32ub,
                           Embedded(PROTOCOL_VERSION),
                           "timestamp" / Embedded(TIMESTAMP))
-                          
+
 VERSIONRESP = Struct("vers_magic" / Int32ub,
                      Embedded(PROTOCOL_VERSION))
-
-ATTRIBUTES = Struct("size" / Int32ub,
-                    "data" / Array(this.size, Byte))
-
-MERKLE_ROOT = Struct("mr_hash" / Array(DEFAULT_HASH_SIZE, Byte))
-
-SLOT_ID = Struct("epoch_index" / VarInt,
-                 "slot_index" / VarInt)
 
 BLOCK_VERSION = Struct("major" / Int16ub,
                        "minor" / Int16ub,
                        "alt" / Int8ub)
 
 # Not a null terminated string
-SOFTWARE_VERSION = Struct("length" / VarInt, 
+SOFTWARE_VERSION = Struct("length" / VarInt,
                           "name" / String(this.length, encoding="utf-8"))
 
 COMMITMENTS_PROOF = Struct("hash_commitments_map" / Array(DEFAULT_HASH_SIZE, Byte),
                            "hash_vss_cert_map" / Array(DEFAULT_HASH_SIZE, Byte))
-                           
+
 OPENINGS_PROOF = Struct("hash_openings_map" / Array(DEFAULT_HASH_SIZE, Byte),
                         "hash_vss_cert_map" / Array(DEFAULT_HASH_SIZE, Byte))
-                        
+
 SHARES_PROOF = Struct("hash_shares_map" / Array(DEFAULT_HASH_SIZE, Byte),
                       "hash_vss_cert_map" / Array(DEFAULT_HASH_SIZE, Byte))
-                      
+
 CERTIFICATES_PROOF = Struct("hash_vss_vert_map" / Array(DEFAULT_HASH_SIZE, Byte))
 
 #TODO: not sure why no attributes yields an 8-byte array of nulls...
@@ -109,8 +205,6 @@ GT_PROOF = Struct("tag" / Int8ub,
                     2:Array(SHARES_PROOF.sizeof(), Byte),
                     3:Array(CERTIFICATES_PROOF.sizeof(), Byte)
                   }))
-
-PUBLIC_KEY = Struct("pub_key" / Array(PUB_KEY_SIZE, Byte))
 
 MAIN_PROOF = Struct("mp_number" / VarInt,
                     Embedded(MERKLE_ROOT),
@@ -142,4 +236,3 @@ PEER_DATA = Struct("nonce" / Array(DEFAULT_NONCE_SIZE, Byte),
                    "protocol_magic" / Int32ub,
                    Embedded(BLOCK_VERSION),
                    "handler_list" / Array(HANDLER_LIST_SIZE, Byte)) # Just throw everything in here...
-
